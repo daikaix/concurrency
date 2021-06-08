3ac14f31f5acba28c45c8ad6d9b69c8c7cfd66c8
daikaix <996327890@qq.com>
2021-06-08 21:47:06 +0800

    StringbufferAndDataFormatter

---
+++ src/main/java/com/mmall/concurrency/example/commonUnsafe/DateFormatExample3.java
@@ -0,0 +1,48 @@
+package com.mmall.concurrency.example.commonUnsafe;
+
+import lombok.extern.slf4j.Slf4j;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+public class DateFormatExample3 {
+    public static int clientTotal = 5000;
+
+    public static int threadTotal = 200;
+
+    private static DateTimeFormatter dateTimeFormat = DateTimeFormat.forPattern("yyyyMMDD");
+
+    public static void main(String[] args) throws Exception{
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for(int i=0; i<clientTotal; i++){
+            final int count = i;
+            executorService.execute(()->{
+                try {
+                    semaphore.acquire();
+                    update(count);
+                    semaphore.release();
+                } catch (Exception e){
+                    log.error("exception",e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+    }
+
+    private static void update(int i){
+        Date date = DateTime.parse("20180801",dateTimeFormat).toDate();
+        log.info("{},{}",i,DateTime.parse("20180801",dateTimeFormat).toDate());
+    }
+}
