9a18433cba7567158af8e741ed93afadc2311ffb
daikaix <996327890@qq.com>
2021-06-09 20:26:03 +0800

    同步容器

---
+++ src/main/java/com/mmall/concurrency/example/syncContainer/VectorExample3.java
@@ -0,0 +1,47 @@
+package com.mmall.concurrency.example.syncContainer;
+
+import com.mmall.concurrency.annotation.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+@Slf4j
+@NotThreadSafe
+public class VectorExample3 {
+    private static void test1(Vector<Integer> v1){
+        for (Integer i: v1){
+            if(i.equals(3)){
+                v1.remove(i);
+            }
+        }
+    }
+
+    private static void test2(Vector<Integer> v1){
+        Iterator<Integer> iter = v1.iterator();
+        while (iter.hasNext()){
+            Integer i = iter.next();
+            if(i.equals(3))
+                v1.remove(i);
+        }
+    }
+
+    //成功的
+    private static void test3(Vector<Integer> v1){
+        for (int i=0; i<v1.size(); i++){
+            if(v1.get(i).equals(3))
+                v1.remove(i);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Vector<Integer> vector = new Vector<>();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        //test1(vector);
+        //test2(vector);
+        test3(vector);
+    }
+
+}
